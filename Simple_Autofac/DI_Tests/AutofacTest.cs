aa01e7a746283bdca125a3fe09fa250c5bc18268
FreDriCk-san <31565053+FreDriCk-san@users.noreply.github.com>
2018-10-20 13:27:18 +0700

    Add files via upload

--- Simple_Autofac/DI_Tests/AutofacTest.cs
+++ Simple_Autofac/DI_Tests/AutofacTest.cs
@@ -4,23 +4,6 @@ using Simple_Autofac;
 
 namespace DI_Tests
 {
-    public class MainClass : IResolve { }
-
-    public interface IResolve { }
-
-
-    public class ConstructorClass : IConstructorResolve
-    {
-        public ConstructorClass(IResolve resolve)
-        {
-
-        }
-    }
-
-    public interface IConstructorResolve { }
-
-
-
 
     [TestClass]
     public class AutofacTest
@@ -29,11 +12,123 @@ namespace DI_Tests
         public void ResolveContainerTest()
         {
             var builder = new ContainerBuilder();
-            builder.RegisterType<MainClass>().As<IResolve>();
+            builder.RegisterType<DefaultClass>().As<IDefaultClassA>();
             builder.Build();
 
-            var instance = builder.Resolve<IResolve>();
-            Assert.IsInstanceOfType(instance, typeof(MainClass));
+            var instance = builder.Resolve<IDefaultClassA>();
+            instance.DefaultWriteA();
+
+            Assert.IsInstanceOfType(instance, typeof(DefaultClass));
+        }
+
+
+
+        [TestMethod]
+        public void NotBuildedException()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DefaultClass>().As<IDefaultClassB>();
+
+            Exception exception = null;
+
+            try
+            {
+                builder.Resolve<IDefaultClassB>();
+            }
+            catch (Exception except)
+            {
+                exception = except;
+            }
+
+            Assert.IsNotNull(exception);
+        }
+
+
+
+        [TestMethod]
+        public void NotRegisteredException()
+        {
+            var builder = new ContainerBuilder();
+            builder.Build();
+
+            Exception exception = null;
+
+            try
+            {
+                builder.Resolve<IDefaultClassA>();
+            }
+            catch (Exception except)
+            {
+                exception = except;
+            }
+
+            Assert.IsNotNull(exception);
+        }
+
+
+
+        [TestMethod]
+        public void ResolveWithConstructorParams()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DefaultClass>().As<IDefaultClassA>();
+            builder.RegisterType<ConstructorClass>().As<IConstructorClass>();
+            builder.Build();
+
+            var instance = builder.Resolve<IConstructorClass>();
+            instance.ConstructorWriteA();
+
+            Assert.IsInstanceOfType(instance, typeof(ConstructorClass));
+        }
+
+
+
+        [TestMethod]
+        public void RegisterAsSelf()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DefaultClass>().AsSelf();
+            builder.Build();
+
+            var instance = builder.Resolve<DefaultClass>();
+            instance.DefaultWriteA();
+            instance.DefaultWriteB();
+
+            Assert.IsInstanceOfType(instance, typeof(DefaultClass));
+        }
+
+
+
+        [TestMethod]
+        public void SetSingletoneByDefault()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DefaultClass>().As<IDefaultClassA>();
+            builder.Build();
+
+            var instance = builder.Resolve<IDefaultClassA>();
+            instance.DefaultWriteA();
+
+            Assert.AreSame(builder.Resolve<IDefaultClassA>(), instance);
+        }
+
+
+
+        [TestMethod]
+        public void CreateTransientInstance()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<DefaultClass>().As<IDefaultClassA>().InstancePerDependency();
+            builder.Build();
+
+            var instance = builder.Resolve<IDefaultClassA>();
+            instance.DefaultWriteA();
+
+            Assert.AreNotSame(builder.Resolve<IDefaultClassA>(), instance);
         }
     }
 }
---
+++ Simple_Autofac/Simple_Autofac/Classes/ContainerBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Simple_Autofac
+{
+    public class ContainerBuilder : IContainer, IRegisteredObject
+    {
+        private IContainer _container;
+        private IRegisteredObject _registeredObject;
+
+        public ContainerBuilder()
+        {
+            this._container = new Container();
+        }
+
+        public IContainer As<TToResolve>()
+        {
+            return _container.As<TToResolve>();
+        }
+
+        public void AsSelf()
+        {
+            _container.AsSelf();
+        }
+
+        public IRegisteredObject Build()
+        {
+            return this._registeredObject = _container.Build();
+        }
+
+        public void InstancePerDependency()
+        {
+            _container.InstancePerDependency();
+        }
+
+        public IContainer RegisterType<TToImplement>()
+        {
+            return _container.RegisterType<TToImplement>();
+        }
+
+        public TTypeToResolve Resolve<TTypeToResolve>()
+        {
+            return _registeredObject.Resolve<TTypeToResolve>();
+        }
+
+        public void SingleInstance()
+        {
+            _container.SingleInstance();
+        }
+    }
+}
